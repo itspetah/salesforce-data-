50c1edb811ddf0c815a2df3d75dea4a70c7260de
Peter <itspeterwang3@gmail.com>
2024-08-22 13:08:38 -0400

    Create credentials.sh

---
+++ csv download/credentials.sh
@@ -0,0 +1,6 @@
+curl https://login.salesforce.com/services/oauth2/token \
+  -d "grant_type=password" \
+  -d "client_id=YOUR_CONSUMER_KEY" \
+  -d "client_secret=YOUR_CONSUMER_SECRET" \
+  -d "username=YOUR_SALESFORCE_USERNAME" \
+  -d "password=YOUR_SALESFORCE_PASSWORDYOUR_SECURITY_TOKEN"
