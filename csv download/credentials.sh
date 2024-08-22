0f80fd902273dcf95d56e751947f6f8803119760
Peter <itspeterwang3@gmail.com>
2024-08-22 13:39:34 -0400

    Delete csv download/credentials.sh

--- csv download/credentials.sh
+++
@@ -1,6 +0,0 @@
-curl https://login.salesforce.com/services/oauth2/token \
-  -d "grant_type=password" \
-  -d "client_id=YOUR_CONSUMER_KEY" \
-  -d "client_secret=YOUR_CONSUMER_SECRET" \
-  -d "username=YOUR_SALESFORCE_USERNAME" \
-  -d "password=YOUR_SALESFORCE_PASSWORDYOUR_SECURITY_TOKEN"
